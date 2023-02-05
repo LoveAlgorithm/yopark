76339d64c1a7e4ec4300571867fb4482c51db01c
Yongjun Park <yopark@student.42seoul.kr>
2023-02-05 21:05:56 +0900

    Revise: 3번

--- 3/3-01404.py
+++ 3/3-01404.py
@@ -17,9 +17,6 @@ for i, (a, b) in enumerate(combinations([i for i in range(8)], 2)):
     prob[b][a] = 1.0 - (l[i] / 100)
 
 
-ans = [0.0] * 8
-
-
 def tournament(case):
     ret = [1.0] * 8
     # 1차전
@@ -60,6 +57,38 @@ def tournament(case):
     return ret
 
 
-ans = tournament([i for i in range(8)])
+def win_prob_r1(a):
+    b = a + 1 if a % 2 == 0 else a - 1
+    return prob[a][b]
+
+
+def win_prob_r2(a):
+    n = a // 2
+    if n == 0:
+        c, d = 2, 3
+    elif n == 1:
+        c, d = 0, 1
+    elif n == 2:
+        c, d = 6, 7
+    else:
+        c, d = 4, 5
+    return win_prob_r1(c) * prob[a][c] + win_prob_r1(d) * prob[a][d]
+
+
+def win_prob_r3(a):
+    n = a // 4
+    if n == 0:
+        e, f, g, h = 4, 5, 6, 7
+    else:
+        e, f, g, h = 0, 1, 2, 3
+    return win_prob_r1(e) * win_prob_r2(e) * prob[a][e] + win_prob_r1(f) * win_prob_r2(f) * prob[a][f] + win_prob_r1(g) * win_prob_r2(g) * prob[a][g] + win_prob_r1(h) * win_prob_r2(h) * prob[a][h]
+
+
+ans = []
+for i in range(8):
+    r1 = win_prob_r1(i)
+    r2 = win_prob_r2(i)
+    r3 = win_prob_r3(i)
+    ans.append(r1 * r2 * r3)
 
 print(*ans)
